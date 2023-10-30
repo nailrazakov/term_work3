0f144aa7482682a674963e9c561e8d8e6c623e99
Nail Razakov Win10 <nailrazakov1982@gmail.com>
2023-10-30 14:05:39 +0300

    логика проверки данных перенесена в main.py, исправлена и протестирована функция записи данных в обьект класса

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -1,6 +1,5 @@
-from src.classes import (Operation, select_state_executed,
-                         reading_information, creating_class_instance)
-import pytest
+from src.classes import (Operation, select_state_executed, reading_information,
+                         creating_class_instance)
 
 """"создаём необходимые для тестирования экземпляры класса Operation"""
 class_instance = Operation()
@@ -16,11 +15,11 @@ operation_5 = Operation(id_transaction="5", state="EXECUTED")
 test_data_class = [operation_1, operation_2, operation_3, operation_4, operation_5]
 
 
-def test_date_formatting():
+def test_date_formating():
     """"
     Тест для метода формата даты класса Operation
     """
-    assert (class_instance.date_formatting("2019-08-26T10:50:58.294041") ==
+    assert (class_instance.date_formating("2019-08-26T10:50:58.294041") ==
             '26-08-2019')
 
 
@@ -32,14 +31,8 @@ def test_masking_information():
 
 def test_information_print():
     assert operation_info.information_print() == ('30-10-2023 ПЕРЕВОД\n'
- 'MAESTRO 1234 12** **** 1234 -> SkyPro 1234 12** **** 1234\n'
- '100 Рублей')
-
-
-def test_creating_class_instance(test_data):
-    assert str(creating_class_instance(test_data)) == ('[Operation Перевод организации 31957.58, Operation Перевод организации '
- '8221.37, Operation Перевод организации 9824.07, Operation Открытие вклада '
- '48223.05, Operation Перевод со счета на счет 79114.93]')
+                                                  'MAESTRO 1234 12** **** 1234 -> SkyPro 1234 12** **** 1234\n'
+                                                  '100 Рублей')
 
 
 def test_reading_information():
@@ -48,3 +41,8 @@ def test_reading_information():
 
 def test_select_state_executed():
     assert len(select_state_executed(test_data_class)) == 3
+
+
+def test_creating_class_instance(test_data):
+    assert (str(creating_class_instance(test_data[0])) ==
+            'Operation Перевод организации 31957.58')
