477cdcc5539508ba069dfa9ec4fd09630f978a19
Nail Razakov Win10 <nailrazakov1982@gmail.com>
2023-10-30 11:42:24 +0300

    упрощена инициаллизация объекта класса, дописан __repr__, изменены имена

--- src/classes.py
+++ src/classes.py
@@ -3,12 +3,13 @@ from datetime import datetime
 
 
 class Operation:
-    def __init__(self, id_transaction, date_of, state, amount, currency, description, where_from,
-                 where_to, currency_code):
+    def __init__(self, id_transaction=None, date_of=None, state=None, amount=None,
+                 currency=None, description=None, where_from=None,
+                 where_to=None, currency_code=None):
         self.id = id_transaction  # id транзакции
         self.date = date_of  # информация о дате совершения операции
         self.state = state  # статус перевода:`EXECUTED` — выполнена, `CANCELED`  — отменена.
-        self.operationAmount = amount  # сумма операции
+        self.operation_amount = amount  # сумма операции
         self.currency = currency  # валюта операции
         self.currency_code = currency_code
         self.description = description  # описание типа перевода
@@ -16,42 +17,34 @@ class Operation:
         self.where_to = where_to  # куда
 
     def __repr__(self):
-        return "Operation"
+        return f"Operation {self.description} {self.operation_amount}"
 
-    def date_formating(self, date):
+    def date_formatting(self, date):
         """
         Преобразует дату в заданный формат ("2019-08-26T10:50:58.294041" -> 26-08-2019)
         """
         return datetime.strptime(date, '%Y-%m-%dT%H:%M:%S.%f').strftime('%d-%m-%Y')
 
-    def where_from_mask(self, where_from):
-        """
-        Маскирует поля класса ("Maestro 1596837868705199" -> Maestro 1596** **** 5199)
-        """
-        if where_from:
-            if "Счет" in where_from:
-                return "Счет **" + where_from[-4:]
-            else:
-                return where_from[:-12] + "** **** " + where_from[-4:]
-        return self.currency_code
 
-    def where_to_mask(self, where_to):
+    def masking_information(self, value):
         """
         Маскирует поле класса ("Maestro 1596837868705199" -> Maestro 1596** **** 5199)
         """
-        if "Счет" in where_to:
-            return "Cчет **" + where_to[-4:]
-        else:
-            return where_to[:-12] + "** **** " + where_to[-4:]
+        if value:
+            if "Счет" in value:
+                return "Счет **" + value[-4:]
+            else:
+                return value[:-12] + "** **** " + value[-4:]
+        return self.currency_code
 
     def information_print(self):
         """
         Собирает данные для вывода в заданном формате
         """
-        return (f'{self.date_formating(self.date)} {self.description}'
-                f'\n{self.where_from_mask(self.where_from)} '
-                f'-> {self.where_to_mask(self.where_to)}'
-                f'\n{self.operationAmount} {self.currency}')
+        return (f'{self.date_formatting(self.date)} {self.description}'
+                f'\n{self.masking_information(self.where_from)} '
+                f'-> {self.masking_information(self.where_to)}'
+                f'\n{self.operation_amount} {self.currency}')
 
 
 def creating_class_instance(data):
@@ -100,7 +93,7 @@ def reading_information(file_name):
 
 
 def select_state_executed(data):
-    """Функция принимает список и по определенным параметрам и значениям возвращает список"""
+    """Функция принимает список и возвращает новый если поле класса "state" - "EXECUTED" """
     new_data = []
     for items in data:
         if items.state == "EXECUTED":
