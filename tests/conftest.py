77d35c3b37faba3750ff0f01999c0aa76c57de22
Nail Razakov Win10 <nailrazakov1982@gmail.com>
2023-10-30 11:59:14 +0300

    подготовили окружение к тестировани, написаны ипройдены тесты

--- tests/conftest.py
+++ tests/conftest.py
@@ -79,20 +79,3 @@ def test_data():
             "to": "Счет 75651667383060284188"
         }]
 
-@pytest.fixture()
-def test_one_operation():
-    return {
-            "id": 142264268,
-            "state": "EXECUTED",
-            "date": "2019-04-04T23:20:05.206878",
-            "operationAmount": {
-                "amount": "79114.93",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод со счета на счет",
-            "from": "Счет 19708645243227258542",
-            "to": "Счет 75651667383060284188"
-        }
---
+++ tests/test_classes.py
@@ -0,0 +1,50 @@
+from src.classes import (Operation, select_state_executed,
+                         reading_information, creating_class_instance)
+import pytest
+
+""""создаём необходимые для тестирования экземпляры класса Operation"""
+class_instance = Operation()
+operation_info = Operation("12345", "2023-10-30T16:43:26.929246",
+                           "EXECUTED", "100", "Рублей",
+                           "ПЕРЕВОД", "MAESTRO 1234 1234 1234 1234",
+                           "SkyPro 1234 1234 1234 1234", "RUB")
+operation_1 = Operation(id_transaction="1", state="EXECUTED")
+operation_2 = Operation(id_transaction="2", state="CANCELED")
+operation_3 = Operation(id_transaction="3", state="EXECUTED")
+operation_4 = Operation(id_transaction="4", state="CANCELED")
+operation_5 = Operation(id_transaction="5", state="EXECUTED")
+test_data_class = [operation_1, operation_2, operation_3, operation_4, operation_5]
+
+
+def test_date_formatting():
+    """"
+    Тест для метода формата даты класса Operation
+    """
+    assert (class_instance.date_formatting("2019-08-26T10:50:58.294041") ==
+            '26-08-2019')
+
+
+def test_masking_information():
+    assert (class_instance.masking_information("Maestro 1596837868705199") ==
+            "Maestro 1596** **** 5199")
+    assert class_instance.masking_information("") == class_instance.currency_code
+
+
+def test_information_print():
+    assert operation_info.information_print() == ('30-10-2023 ПЕРЕВОД\n'
+ 'MAESTRO 1234 12** **** 1234 -> SkyPro 1234 12** **** 1234\n'
+ '100 Рублей')
+
+
+def test_creating_class_instance(test_data):
+    assert str(creating_class_instance(test_data)) == ('[Operation Перевод организации 31957.58, Operation Перевод организации '
+ '8221.37, Operation Перевод организации 9824.07, Operation Открытие вклада '
+ '48223.05, Operation Перевод со счета на счет 79114.93]')
+
+
+def test_reading_information():
+    assert len(reading_information("../operations.json")) > 0
+
+
+def test_select_state_executed():
+    assert len(select_state_executed(test_data_class)) == 3
